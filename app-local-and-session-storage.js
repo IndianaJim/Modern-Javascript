34d52a0d9a84836e705f90050e8537e9baf25e30
indianajim <jimrunkel@gmail.com>
2018-09-01 20:56:06 -0400

    local and session storage

---
+++ app-local-and-session-storage.js
@@ -0,0 +1,45 @@
+// local and session storage
+
+// //set local storage item
+// localStorage.setItem('name', 'John');
+// localStorage.setItem('age', '30');
+
+// //set session storage item
+// sessionStorage.setItem('name', 'Kelly');
+
+//remove from storage
+//localStorage.removeItem('name');
+
+// //get from storage
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age');
+
+// //clear local storage
+// localStorage.clear();
+
+// console.log(name, age);
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  //console.log(123);
+  const task = document.getElementById('task').value;
+
+  let tasks;
+
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(function(task) {
+  console.log(task);
+});
